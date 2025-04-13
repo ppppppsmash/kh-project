8d53b91865f22d2ef6e29f7d176fe3b705191134
kurosawa <pppppp.work@gmail.com>
2025-04-14 00:01:23 +0900

    fix: Fix middleware for test

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { type NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ 
     req: request,
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: "test",
   });
   const isAuthenticated = !!token;
 
