77b2d473a23743a71e8ea6bd9df5e432bce788d9
kurosawa <pppppp.work@gmail.com>
2025-04-16 09:21:43 +0900

    chore: Install vercel/blob for image upload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['kxiebh2k4xk247kc.public.blob.vercel-storage.com'],
+  },
 };
 
 export default nextConfig;
